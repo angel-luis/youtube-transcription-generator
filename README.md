33fe501ddf0eb1a7ee6efe3dc50a0827dff64990
angel-luis <angel.luis@me.com>
2023-08-06 16:52:19 +0200

    Simple server with Title generation input and response from LLM

--- README.md
+++ README.md
@@ -1,9 +1,9 @@
 A simple YouTube transcriptions generator using OpenAI GPT-3.5.
 
-Full guide: https://www.youtube.com/watch?v=MlK6SIjcjE8
+This is not intended for final user usage, is just for learning `LangChain`. Full guide: https://www.youtube.com/watch?v=MlK6SIjcjE8
 
 # Usage
 
 Create an `.env` file filling your `OPENAI_API_KEY`. Configure an environment profile to be able to load the `.env` file.
 
-Start the `pipenv shell` to execute the script.
+Start the `pipenv shell` and then `streamlit run main.py` to run the server.
--- main.py
+++ main.py
@@ -1,3 +1,25 @@
-import os
+import streamlit as st
+from langchain.llms import OpenAI
+from langchain.prompts import PromptTemplate
+from langchain.chains import LLMChain
 
-print(os.environ["OPENAI_API_KEY"])
+# Web App
+st.title("🎬 YouTube Transcription Generator")
+title_input = st.text_input("What is the video about")
+
+# Prompt
+title_template = PromptTemplate(
+    input_variables=["topic"],
+    template="write me a shocking title for a YouTube video about {topic}"
+)
+
+# LLM
+llm = OpenAI(temperature=0.7)
+
+# Chain
+title_chain = LLMChain(llm=llm, prompt=title_template)
+
+# Show if title_input submitted
+if title_input:
+    response = title_chain.run(title_input)
+    st.write(response)
